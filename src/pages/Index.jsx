18d971f79dfc9b79c31c0307b72f8d1a660f468f
Marco Alarcon <markinsh0@gmail.com>
2023-11-06 22:23:03 -0500

    Avanzando el index page

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -3,8 +3,8 @@ import Slider from "react-slick"
 import { AutoSlider } from "../components/AutoSlider"
 import { Tarjeta } from '../components/Tarjeta'
 import { obtenerPromocionesOnline, obtenerListadoMenu } from '../data/bembosAPI'
-import '../styles/listados.css'
 import '../styles/tarjetas.css'
+import '../styles/secciones.css'
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
 
@@ -30,8 +30,8 @@ export const Index = () => {
   return (
     <>
       <AutoSlider />
-      <section className="contenedor">
-        <h2>Promociones de hamburguesas online</h2>
+      <section className="seccion__promociones__online contenedor">
+        <h2 className="promociones__online__titulo">Promociones de hamburguesas online</h2>
         <Slider {...settings}>
           {promociones.map(promo => (
             <Tarjeta>
@@ -52,9 +52,9 @@ export const Index = () => {
           ))}
         </Slider>
       </section>
-      <section>
-        <h2>Menu de hamburguesas online</h2>
-        <Slider>
+      <section className="seccion__menuburguer__online contenedor">
+        <h2 className="menuburguer__online__titulo">Menu de hamburguesas online</h2>
+        <Slider {...settings}>
           {listadoMenu.map(itemMenu => (
             <Tarjeta>
               <div className="tarjeta__contenedor__imagen">
@@ -62,7 +62,7 @@ export const Index = () => {
               </div>
               <div className="tarjeta__contenido">
                 <div className="tarjeta__contenido__informacion">
-                  <h4 className="tarjeta__titulo">{itemMenu.nombre}</h4>
+                  <h4 className="tarjeta__titulo tarjeta__titulo__menuburguer">{itemMenu.nombre}</h4>
                 </div>
                 <button className="tarjeta__boton">Ver Todos</button>
               </div>
