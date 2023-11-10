488eaa7b8e3a588960533323625d6cf546fe74b0
Marco Alarcon <markinsh0@gmail.com>
2023-11-10 03:40:32 -0500

    Scroll al cambiar pagina

--- src/components/templates/LayoutPrincipal.jsx
+++ src/components/templates/LayoutPrincipal.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import { Header } from "../Header"
 import { Footer } from "../Footer"
+import { ScrollToTop } from '../../components/ScrollToTop'
 
 export const LayoutPrincipal = () => {
     return (
         <>
+            <ScrollToTop />
             <Header />
             <main className="principal">
                 <div className="contenedor__boton__sugerencias">
